d465ffac52fb2f9e35ded3ee10bc7bb5dc8549d7
kkorekk <kornel.maleszka@stxnext.pl>
2017-11-15 09:33:21 +0100

    CDS-668 Add link to the top build in chain

--- master/buildbot/status/web/build.py
+++ master/buildbot/status/web/build.py
@@ -26,6 +26,7 @@ from buildbot.status.web.base import HtmlResource, \
      getAndCheckProperties, ActionResource, path_to_authzfail, \
      getRequestCharset, path_to_json_build
 from buildbot.schedulers.forcesched import ForceScheduler, TextParameter
+from buildbot.status.builder import BuilderStatus
 from buildbot.status.web.status_json import BuildJsonResource
 from buildbot.status.web.step import StepsResource
 from buildbot.status.web.tests import TestsResource
@@ -290,7 +291,21 @@ class StatusResourceBuild(HtmlResource):
         cxt['build_url'] = path_to_build(req, b, False)
         cxt['slave_debug_url'] = self.getBuildmaster(req).config.slave_debug_url
         cxt['customBuildUrls'] = b.getCustomUrls()
+        cxt['build_chain_top_build_url'] = None
         codebases_arg = cxt['codebases_arg'] = getCodebasesArg(request=req)
+
+        build_chain_id = b.buildChainID
+        if build_chain_id is not None:
+            status = self.getStatus(req)
+
+            build_chain_build_request = yield b.master.db.buildrequests.getBuildRequestById(b.buildChainID)
+            build_chain_builder = status.getBuilder(build_chain_build_request['buildername'])
+
+            build_chain_build_number = yield b.master.db.builds.getBuildNumberForRequest(b.buildChainID)
+            build_chain_build_status = build_chain_builder.getBuildByNumber(build_chain_build_number)
+
+            if self.build_status != build_chain_build_status:
+                cxt['build_chain_top_build_url'] = path_to_build(req, build_chain_build_status)
 
         if not b.isFinished():
             cxt['stop_build_chain'] = False
