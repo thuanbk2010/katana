574977f1f46b1faa1422da99185f2f304f10430f
Christian Bay <christianb@unity3d.com>
2017-11-20 16:50:28 +0100

    Extract out common logic for the build forms

--- master/buildbot/status/web/forms.py
+++ master/buildbot/status/web/forms.py
@@ -38,8 +38,53 @@ class FormsKatanaResource(HtmlResource):
         elif path == "rebuild":
             return RebuildDialogPage()
 
+class BuildDialogPage(HtmlResource):
+    def _decodeArgs(self, request, encoding):
+        args = request.args.copy()
+        for name, argl in args.iteritems():
+            if '_branch' in name:
+                args[name] = [ self.decodeFromURL(arg, encoding) for arg in argl ]
+        return args
 
-class ForceBuildDialogPage(HtmlResource):
+    def _getSlaves(self, builder_status):
+        slaves = [s for s in builder_status.getSlaves() if s.isConnected()]
+        slaves = sorted(slaves, key=attrgetter('friendly_name'))
+        return slaves
+
+    def _getBranches(self, args, request):
+        encoding = getRequestCharset(request)
+        branches = [b.decode(encoding) for b in args.get("branch", []) if b]
+        return branches
+
+    @defer.inlineCallbacks
+    def _getIsAdmin(self, request):
+        authz = self.getAuthz(request)
+        isAdmin = yield authz.getUserAttr(request, 'is_admin', 0)
+        defer.returnValue(isAdmin)
+
+    def _getReturnPage(self, args, url_parts):
+        return_page = ""
+        if args.has_key("return_page"):
+            return_page = args['return_page']
+            if not isinstance(return_page, basestring):
+                return_page = args['return_page'][0]
+
+        # TODO: This is wrong and buggy
+        if len(url_parts) > 0 and len(return_page) > 0:
+            return_page = "&returnpage={0}".format(return_page)
+        else:
+            return_page = "?returnpage={0}".format(return_page)
+
+        return return_page
+
+    def _getForceUrl(self, return_page, url_parts):
+        url_parts[2] += "/force"
+        url_parts[4] += return_page
+        force_url = urlunparse(url_parts)
+        return force_url
+
+
+class ForceBuildDialogPage(BuildDialogPage):
     pageTitle = "Force Build"
 
     def decodeFromURL(self, value, encoding):
@@ -48,55 +93,25 @@ class ForceBuildDialogPage(HtmlResource):
     @defer.inlineCallbacks
     def content(self, request, cxt):
         status = self.getStatus(request)
-
-        args = request.args.copy()
-
-        # decode all of the args
         encoding = getRequestCharset(request)
-        for name, argl in args.iteritems():
-            if '_branch' in name:
-                args[name] = [ self.decodeFromURL(arg, encoding) for arg in argl ]
+        args = self._decodeArgs(request, encoding)
 
         #Get builder info
         builder_status = None
         if args.has_key("builder_name") and len(args["builder_name"]) == 1:
             builder_status = status.getBuilder(self.decodeFromURL(args["builder_name"][0], encoding))
-            bm = self.getBuildmaster(request)
+            buildMaster = self.getBuildmaster(request)
 
-            cxt['slaves'] = [s for s in builder_status.getSlaves() if s.isConnected()]
-            cxt['slaves'] = sorted(cxt['slaves'], key=attrgetter('friendly_name'))
+            cxt['slaves'] = self._getSlaves(builder_status)
+            cxt['branches'] = self._getBranches(args, request)
 
-            #Get branches
-            encoding = getRequestCharset(request)
-            branches = [ b.decode(encoding) for b in args.get("branch", []) if b ]
-            cxt['branches'] = branches
+            buildForceContext(cxt, request, buildMaster, builder_status.getName())
 
-            return_page = ""
-            if args.has_key("return_page"):
-                return_page = args['return_page']
-                if not isinstance(return_page, basestring):
-                    return_page = args['return_page'][0]
+            url_parts = list(urlparse(args['builder_url'][0]))
+            cxt['return_page'] = self._getReturnPage(args, url_parts)
+            cxt['force_url'] = self._getForceUrl(cxt['return_page'], url_parts)
 
-            #Add scheduler info
-            buildForceContext(cxt, request, self.getBuildmaster(request), builder_status.getName())
-
-            #URL to force page with return param
-            url = args['builder_url'][0]
-            url_parts = list(urlparse(url))
-
-            if len(url_parts) > 0 and len(return_page) > 0:
-                return_page = "&returnpage={0}".format(return_page)
-            else:
-                return_page = "?returnpage={0}".format(return_page)
-            cxt['return_page'] = return_page
-
-            url_parts[2] += "/force"
-            url_parts[4] += return_page
-            force_url = urlunparse(url_parts)
-            cxt['force_url'] = force_url
-
-            authz = self.getAuthz(request)
-            cxt['is_admin'] = yield authz.getUserAttr(request, 'is_admin', 0)
+            cxt['is_admin'] = yield self._getIsAdmin(request)
             cxt['rt_update'] = args
             request.args = args
 
@@ -107,7 +122,7 @@ class ForceBuildDialogPage(HtmlResource):
             page = ErrorPage(INTERNAL_SERVER_ERROR, "Missing parameters", "Not all parameters were given")
             defer.returnValue(page.render(request))
 
-class RebuildDialogPage(HtmlResource):
+class RebuildDialogPage(BuildDialogPage, HtmlResource):
     pageTitle = "Rebuild"
 
     def decodeFromURL(self, value, encoding):
@@ -116,14 +131,8 @@ class RebuildDialogPage(HtmlResource):
     @defer.inlineCallbacks
     def content(self, request, cxt):
         status = self.getStatus(request)
-
-        args = request.args.copy()
-
-        # decode all of the args
         encoding = getRequestCharset(request)
-        for name, argl in args.iteritems():
-            if '_branch' in name:
-                args[name] = [ self.decodeFromURL(arg, encoding) for arg in argl ]
+        args = self._decodeArgs(request, encoding)
 
         # Get build info
         buildNumber = int(self._getSingleArgument(args, encoding, "build_number"))
@@ -132,46 +141,19 @@ class RebuildDialogPage(HtmlResource):
         if buildNumber != None and builderName != None:
             builder_status = status.getBuilder(builderName)
             build = builder_status.getBuild(buildNumber)
+            buildMaster = self.getBuildmaster(request)
 
-            bm = self.getBuildmaster(request)
+            cxt['slaves'] = self._getSlaves(builder_status)
+            cxt['branches'] = self._getBranches(args, request)
 
-            cxt['slaves'] = [s for s in builder_status.getSlaves() if s.isConnected()]
-            cxt['slaves'] = sorted(cxt['slaves'], key=attrgetter('friendly_name'))
+            buildForceContext(cxt, request, buildMaster, builder_status.getName())
+            self._overrideForceContext(request, cxt, build, buildMaster, builderName)
 
-            #Get branches
-            encoding = getRequestCharset(request)
-            branches = [ b.decode(encoding) for b in args.get("branch", []) if b ]
-            cxt['branches'] = branches
+            url_parts = list(urlparse(args['builder_url'][0]))
+            cxt['return_page'] = self._getReturnPage(args, url_parts)
+            cxt['force_url'] = self._getForceUrl(cxt['return_page'], url_parts)
 
-            return_page = ""
-            if args.has_key("return_page"):
-                return_page = args['return_page']
-                if not isinstance(return_page, basestring):
-                    return_page = args['return_page'][0]
-
-            #Add scheduler info
-            buildForceContext(cxt, request, bm, builder_status.getName())
-
-            #Override force context
-            self._overrideForceContext(request, cxt, build, bm, builderName)
-
-            #URL to force page with return param
-            url = args['builder_url'][0]
-            url_parts = list(urlparse(url))
-
-            if len(url_parts) > 0 and len(return_page) > 0:
-                return_page = "&returnpage={0}".format(return_page)
-            else:
-                return_page = "?returnpage={0}".format(return_page)
-            cxt['return_page'] = return_page
-
-            url_parts[2] += "/force"
-            url_parts[4] += return_page
-            force_url = urlunparse(url_parts)
-            cxt['force_url'] = force_url
-
-            authz = self.getAuthz(request)
-            cxt['is_admin'] = yield authz.getUserAttr(request, 'is_admin', 0)
+            cxt['is_admin'] = yield self._getIsAdmin(request)
             cxt['rt_update'] = args
             request.args = args
 
