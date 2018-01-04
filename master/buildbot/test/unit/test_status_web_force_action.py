8f8c717d52d86e21163b9e18da812aa816979855
kkorekk <kornel.maleszka@stxnext.pl>
2018-01-04 17:18:25 +0100

    remove scheduler_factory from TestForceAction

--- master/buildbot/test/unit/test_status_web_force_action.py
+++ master/buildbot/test/unit/test_status_web_force_action.py
@@ -29,15 +29,14 @@ class TestForceAction(unittest.TestCase):
         self.force_action_resource.builder_status = self.builder_status
 
     @defer.inlineCallbacks
-    def test_forcescheduler_param(self):
-        first_scheduler = self._scheduler_factory(
-            spec=ForceScheduler, name='test-scheduler-0+[force]',
-        )
-        self.master.scheduler_manager.addService(first_scheduler)
+    def test_forcescheduler_form_param(self):
+        first_scheduler = mock.Mock(spec=ForceScheduler)
+        first_scheduler.name = 'test-scheduler-0+[force]'
 
-        selected_scheduler = self._scheduler_factory(
-            spec=ForceScheduler, name='test-scheduler-1+[force]',
-        )
+        selected_scheduler = mock.Mock(spec=ForceScheduler)
+        selected_scheduler.name = 'test-scheduler-1+[force]'
+
+        self.master.scheduler_manager.addService(first_scheduler)
         self.master.scheduler_manager.addService(selected_scheduler)
 
         self.request.addArg('forcescheduler', selected_scheduler.name)
@@ -47,27 +46,15 @@ class TestForceAction(unittest.TestCase):
         selected_scheduler.force.assert_called_once()
         first_scheduler.force.assert_not_called()
 
-    @staticmethod
-    def _scheduler_factory(spec, **kwargs):
-        scheduler = mock.Mock(spec=spec)
-
-        for attribute, value in kwargs.items():
-            setattr(scheduler, attribute, value)
-
-        return scheduler
-
     @defer.inlineCallbacks
-    def test_forcescheduler_param_empty(self):
-        first_scheduler = self._scheduler_factory(
-            spec=ForceScheduler, name='test-scheduler-0+[force]', builderNames=[],
-        )
-        self.master.scheduler_manager.addService(first_scheduler)
+    def test_forcescheduler_form_param_empty(self):
+        first_scheduler = mock.Mock(spec=ForceScheduler, builderNames=[])
+        first_scheduler.name = 'test-scheduler-0+[force]'
 
-        selected_scheduler = self._scheduler_factory(
-            spec=ForceScheduler,
-            name='test-scheduler-1+[force]',
-            builderNames=[self.builder_name],
-        )
+        selected_scheduler = mock.Mock(spec=ForceScheduler, builderNames=[self.builder_name])
+        selected_scheduler.name = 'test-scheduler-1+[force]'
+
+        self.master.scheduler_manager.addService(first_scheduler)
         self.master.scheduler_manager.addService(selected_scheduler)
 
         yield self.force_action_resource.force(self.request, [self.builder_name])
@@ -76,19 +63,14 @@ class TestForceAction(unittest.TestCase):
         first_scheduler.force.assert_not_called()
 
     @defer.inlineCallbacks
-    def test_forcescheduler_param_empty_only_force_schedulers_allowed(self):
-        first_scheduler = self._scheduler_factory(
-            spec=BaseScheduler,
-            name='test-scheduler-0+[force]',
-            builderNames=[self.builder_name],
-        )
-        self.master.scheduler_manager.addService(first_scheduler)
+    def test_forcescheduler_form_param_empty_only_force_schedulers_allowed(self):
+        first_scheduler = mock.Mock(spec=BaseScheduler, builderNames=[self.builder_name])
+        first_scheduler.name = 'test-scheduler-0+[force]'
 
-        selected_scheduler = self._scheduler_factory(
-            spec=ForceScheduler,
-            name='test-scheduler-1+[force]',
-            builderNames=[self.builder_name],
-        )
+        selected_scheduler = mock.Mock(spec=ForceScheduler, builderNames=[self.builder_name])
+        selected_scheduler.name = 'test-scheduler-1+[force]'
+
+        self.master.scheduler_manager.addService(first_scheduler)
         self.master.scheduler_manager.addService(selected_scheduler)
 
         yield self.force_action_resource.force(self.request, [self.builder_name])
@@ -96,12 +78,10 @@ class TestForceAction(unittest.TestCase):
         selected_scheduler.force.assert_called_once()
 
     @defer.inlineCallbacks
-    def test_forcescheduler_param_empty_scheduler_not_found(self):
-        scheduler = self._scheduler_factory(
-            spec=ForceScheduler,
-            name='test-scheduler-0+[force]',
-            builderNames=[],
-        )
+    def test_forcescheduler_form_param_empty_scheduler_not_found(self):
+        scheduler = mock.Mock(spec=ForceScheduler, builderNames=[])
+        scheduler.name = 'test-scheduler-0+[force]'
+
         self.master.scheduler_manager.addService(scheduler)
 
         result = yield self.force_action_resource.force(self.request, [self.builder_name])
@@ -115,6 +95,8 @@ class TestForceAction(unittest.TestCase):
         )
 
     def test_decode_request_arguments(self):
-        self.request.addArg('checkbox', 'checkbox-selected')
+        self.request.args['checkbox'] = ['checkbox-selected-1', 'checkbox-selected-2']
+
         args = ForceAction.decode_request_arguments(self.request)
-        self.assertEqual(args, {'checkbox-selected': True})
+
+        self.assertEqual(args, {'checkbox-selected-1': True, 'checkbox-selected-2': True})
